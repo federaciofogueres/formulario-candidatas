be8227585f24c8c5849a2749208a7a52e9e5f861
Juan Daniel <juandablasco@gmail.com>
2017-10-23 18:31:21 +0200

    Cambio menor en el dni

--- index.php
+++ index.php
@@ -131,8 +131,7 @@ if (!$isCandidata) {
                             <div class="col-md-4">
                                 <div class="form-group label-floating">
                                     <label class="control-label">DNI</label>
-                                    <input type="text" class="form-control" id="dni" name="dni" pattern="^\d{8}[a-zA-Z]$" maxlength="9"
-                                           title="Formato: 12345678A" required>
+                                    <input type="text" class="form-control" id="dni" name="dni" pattern="^\d{8}[a-zA-Z]$" maxlength="9" required>
                                     <p class="text-danger" id="mensajeErrorDni">DNI incorrecto</p>
                                 </div>
                             </div>
